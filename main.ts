6d440f5e805aee1ef63ad72e0bae06c75daf9ca3
WilliamWu-MS <93303494+WilliamWu-MS@users.noreply.github.com>
2021-12-17 11:20:23 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showLeds(`
+        . . # # .
+        # . # . #
+        . # # # .
+        # . # . #
+        . . # # .
+        `)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+bluetooth.startButtonService()
+bluetooth.startUartService()
+basic.forever(function () {
+	
+})
